191bec63022983fabce45aead064cb382d53340d
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2021-07-07 21:07:13 +0530

    second commit

--- regex/regestut.js
+++ regex/regestut.js
@@ -4,3 +4,7 @@ let myregex = /Hello/;  // yaha pe quotation nhi aaenge this var contains to fin
 let result = myregex.test(myString); // jiska check karna hai jo / word / mein hai woh pehle aata hai
 
 console.log(result);
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // | line is basically OR agar in 4 words se koi bhi mil jae toh balle balle (true return kardena)
+let result = petRegex.test(petString);
