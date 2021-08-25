771c1051d49dad3e7b6b6b193d4d97a761505248
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2021-08-25 09:41:03 +0530

    deploy-command.js file added

--- JS projects/DiscordBotProject/index.js
+++ JS projects/DiscordBotProject/index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
-import { Client, Intents } from "discord.js";
-import { token } from "./config.json";
+const { Client, Intents } = require("discord.js");
+const { token } = require("./config.json");
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -8,6 +8,25 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
   console.log("Ready!");
+});
+
+//  To listen for those events, we need to add a listener to the client
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+    );
+  } else if (commandName === "user") {
+    await interaction.reply(
+      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+    );
+  }
 });
 
 // Login to Discord with your client's token
